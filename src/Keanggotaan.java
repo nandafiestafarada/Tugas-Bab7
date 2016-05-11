21c9fb963433b41fcf2926e68c3b21b315fa6efa
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:30:44 +0700

    konstruktor

--- src/Keanggotaan.java
+++ src/Keanggotaan.java
@@ -10,3 +10,12 @@ public abstract class Keanggotaan {
     public double hargajasa;
     public double hargaProduk;
     public String nama;
+    
+    public Keanggotaan(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
+        this.produk = produk;
+        this.jasa = jasa;
+        this.hargaProduk = hargaProduk;
+        this.hargajasa = hargajasa;
+        this.nama = nama;
+    }
+
