980c3387352c4e35ecb7d927a67a105c0ad43e97
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:57:11 +0700

    konstruk

--- src/Premium.java
+++ src/Premium.java
@@ -1,1 +1,9 @@
+package pllaporan7;
 
+public class Premium extends Member {
+
+    double diskon;
+
+    public Premium(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
+        super(produk, jasa, hargaProduk, hargajasa, nama);
+    }
