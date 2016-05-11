74b2ff68252b920b5a181eb36095cde802fe793f
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:43:48 +0700

    knstruktor

--- src/BukanMember.java
+++ src/BukanMember.java
@@ -1,1 +1,9 @@
+package pllaporan7;
 
+public class BukanMember extends Keanggotaan {
+
+    double diskon;
+
+    public BukanMember(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
+        super(produk, jasa, hargaProduk, hargajasa, nama);
+    }
