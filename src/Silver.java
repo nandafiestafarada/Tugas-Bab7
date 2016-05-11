f032547fd02ae5d1243cab01cc1b5fd74ec8e56c
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 23:25:12 +0700

    fihmxreyfgcurg

--- src/Silver.java
+++ src/Silver.java
@@ -1,1 +1,32 @@
+package pllaporan7;
+
+public class Silver extends Member {
+
+    double diskon;
+
+    public Silver(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
+        super(produk, jasa, hargaProduk, hargajasa, nama);
+    }
+
+    public double diskonlayanan() {
+        return 0.1;
+    }
+
+    public double diskonproduk() {
+        return super.diskonproduk();
+    }
+
+    public void anggota() {
+        super.anggota();
+        System.out.println("Member anda adalah SILVER");
+        System.out.println("Diskon Pelayanan sebesar 10 % jika melakukan perawatn");
+        System.out.println("Diskon Produk sebesar 10 % jika membeli produk");
+    }
+
+    public double bayar() {
+        double total;
+        total = super.bayar() - super.getHargaPelayanan() * diskonlayanan();
+        return total;
+    }
+}
 
