ff230949ecf2c363807895e7e60a8744097cf6c8
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:30:08 +0700

    atribut

--- src/Keanggotaan.java
+++ src/Keanggotaan.java
@@ -1,1 +1,12 @@
+package pllaporan7;
 
+public abstract class Keanggotaan {
+
+    public abstract double diskonlayanan();
+
+    public abstract double diskonproduk();
+    public String produk;
+    public String jasa;
+    public double hargajasa;
+    public double hargaProduk;
+    public String nama;
