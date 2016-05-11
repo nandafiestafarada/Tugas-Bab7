e0cfc05f16a7a72c904f2021fa6d1df494b09dc2
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:42:25 +0700

    yoiki

--- src/Member.java
+++ src/Member.java
@@ -5,3 +5,20 @@ public class Member extends Keanggotaan {
     public Member ( String produk, String jasa, double hargaProduk, double hargajasa, String nama){
         super( produk, jasa, hargaProduk, hargajasa, nama);
     } 
+    
+    public double diskonproduk(){
+        return 0.1;
+    }
+    public void anggota (){
+        System.out.println("Anda masuk dalam anggota member ");
+    }
+      @Override
+    public double diskonlayanan() {
+        return 0;
+    }
+       public double bayar(){
+      double total;
+      total = super.bayar()- super.getHargaProduk()*diskonproduk();
+      return total;
+    }
+}
