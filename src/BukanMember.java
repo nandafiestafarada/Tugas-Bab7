60757a47b42e581fa46dd14f0430ff201ab88809
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:44:29 +0700

    ovverride method

--- src/BukanMember.java
+++ src/BukanMember.java
@@ -7,3 +7,23 @@ public class BukanMember extends Keanggotaan {
     public BukanMember(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
         super(produk, jasa, hargaProduk, hargajasa, nama);
     }
+    
+    public double diskonlayanan() {
+        return 0;
+    }
+
+    public double diskonproduk() {
+        diskon = 0;
+        return diskon;
+    }
+
+    public void anggota() {
+        System.out.println("Anda bukan termasuk Member, tidak ada diskon untuk Anda");
+    }
+
+    public double bayar() {
+        double total;
+        total = super.bayar() - super.getHargaProduk() * diskonproduk();
+        return total;
+    }
+}
