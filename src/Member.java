83032e3fdd27de27d1873e29b22e0836e3cf7777
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 22:41:15 +0700

    konstruktor

--- src/Member.java
+++ src/Member.java
@@ -1,1 +1,7 @@
 
+package pllaporan7;
+
+public class Member extends Keanggotaan {  
+    public Member ( String produk, String jasa, double hargaProduk, double hargajasa, String nama){
+        super( produk, jasa, hargaProduk, hargajasa, nama);
+    } 
