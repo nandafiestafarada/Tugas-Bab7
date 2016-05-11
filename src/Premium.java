38351b53352539dedb90a203a41e612003b69e4b
nandafiestafarada <nandafiestafarada@gmail.com>
2016-05-11 23:23:36 +0700

    premium

--- src/Premium.java
+++ src/Premium.java
@@ -7,3 +7,26 @@ public class Premium extends Member {
     public Premium(String produk, String jasa, double hargaProduk, double hargajasa, String nama) {
         super(produk, jasa, hargaProduk, hargajasa, nama);
     }
+    
+    public double diskonlayanan() {
+        return 0.2;
+    }
+
+    public double diskonproduk() {
+        return super.diskonproduk();
+    }
+
+    public void anggota() {
+        super.anggota();
+        System.out.println("Member anda adalah PREMIUM");
+        System.out.println("Diskon Pelayanan sebesar 20 % jika melakukan perawatan");
+        System.out.println("Diskon Produk sebesar 10 % jika membeli produk");
+    }
+
+    public double bayar() {
+        double total;
+        total = super.bayar() - super.getHargaPelayanan() * diskonlayanan();
+        return total;
+    }
+}
+
